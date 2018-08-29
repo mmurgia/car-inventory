b8279d4e37558b1ce54a9b6a58e5532862b10553
Matthew Murgia <mmurgia@ms3-inc.com>
2018-08-29 14:17:03 -0400

    update creation of admin user to property files

--- server/boot/create-users-and-roles.js
+++ server/boot/create-users-and-roles.js
@@ -1,34 +1,36 @@
-// module.exports = function(app) {
-//   var User = app.models.user;
-//   var Role = app.models.Role;
-//   var RoleMapping = app.models.RoleMapping;
-//
-//   User.create([
-//     {username: 'mmurgia', email: 'mmurgia@doe.com', password: 'password'},
-//     {username: 'Jane', email: 'jane@doe.com', password: 'password'},
-//     {username: 'Bob', email: 'bob@projects.com', password: 'password'}
-//   ], function(err, users) {
-//     if (err) throw err;
-//
-//     console.log('Created users:', users);
-//
-//     //create the admin role
-//     Role.create({
-//       name: 'admin'
-//     }, function(err, role) {
-//       if (err) throw err;
-//
-//       console.log('Created role:', role);
-//
-//       //make bob an admin
-//       role.principals.create({
-//         principalType: RoleMapping.USER,
-//         principalId: users[0].id
-//       }, function(err, admin) {
-//         if (err) throw err;
-//
-//         console.log('Created admin:', admin);
-//       });
-//     });
-//   });
-// };
+module.exports = function(app) {
+  var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+
+  User.create([
+    {
+      username: process.env.USERNAME,
+      email: process.env.EMAIL,
+      password: process.env.PASSWORD
+    }
+  ], function(err, users) {
+    if (err) throw err;
+
+    console.log('Created users:', users);
+
+    //create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) throw err;
+
+      console.log('Created role:', role);
+
+      //make bob an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[0].id
+      }, function(err, admin) {
+        if (err) throw err;
+
+        console.log('Created admin:', admin);
+      });
+    });
+  });
+};
